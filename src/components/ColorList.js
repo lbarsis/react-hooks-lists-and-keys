1ef85799e5fae53ead37cd879881570e15a4d5f3
Levi Barsis <lbarsis@gmail.com>
2022-10-01 11:45:16 -0700

    Done with assignment (no tests)

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -9,15 +9,16 @@ function ColorList() {
     "hotpink",
   ];
 
+  const styles = colors.map(styleEl => {
+    return <li style={{ color: styleEl }}>{styleEl}</li>
+  })
+
   return (
+
     <div>
       <h1>Top 5 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {styles}
       </ol>
     </div>
   );
